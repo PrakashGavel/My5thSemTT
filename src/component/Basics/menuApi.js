c097bff8f44708d9ded337cc0466afc5f6bc0f75
PrakashGavel <prakashgavel1000@gmail.com>
2022-07-22 18:25:08 +0530

    Initial commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,170 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/Blockchain.jpg",
+      name: "Blockchain and Cryptocurr Tech",
+      category: "Monday",
+      description:
+        "2:00 PM - 2:50 PM - SJT415 - A2+TA2 - Poornima N",
+    },
+    {
+      id: 2,
+      image: "images/ISAA.png",
+      name: "Info Security Analysis and Audit",
+      category: "Monday",
+      description:
+        "3:00 PM - 3:50 PM - F2 - SJT208 - Sivakumar N",
+    },
+    {
+      id: 3,
+      image: "images/STS.jpg",
+      name: "STS",
+      category: "Monday",
+      description:
+        "4:00 PM - 4:50 PM - D2+TD2 - SJT423",
+    },
+
+    {
+      id: 1,
+      image: "images/German.jpg",
+      name: "German",
+      category: "Tuesday",
+      description:
+        "9:00 AM - 9:50 AM - G1 - SJT126 - Dhananjay Kumar",
+    },
+    {
+      id: 2,
+      image: "images/ISAA.png",
+      name: "Info Security Analysis and Audit (Lab)",
+      category: "Tuesday",
+      description:
+        "9:50 AM - 11:30 AM - L9 + L10 - SJT317 - Sivakumar N",
+    },
+    {
+      id: 3,
+      image: "images/SE.jpg",
+      name: "Software Engineering",
+      category: "Tuesday",
+      description:
+        "2:00 PM - 2:50 PM - B2 - SJT311 - Anto S",
+    },
+    {
+      id: 4,
+      image: "images/AI.jpg",
+      name: "Artificial Intelligence",
+      category: "Tuesday",
+      description:
+        "4:00 PM - 4:50 PM - E2+TE2 - SJT617 - Tusar Kanti Mishra",
+    },
+
+    {
+      id: 1,
+      image: "images/SE.jpg",
+      name: "Software Engineering (Lab)",
+      category: "Wednesday",
+      description:
+        "8:00 AM - 9:40 AM - L13 + L14 - SJT417 - Anto S",
+    },
+    {
+      id: 2,
+      image: "images/IWP.jpeg",
+      name: "Internet and Web Programming",
+      category: "Wednesday",
+      description:
+        "2:00 PM - 2:50 PM - C2 - SJT304 - Rajkumar R",
+    },
+    {
+      id: 3,
+      image: "images/Blockchain.jpg",
+      name: "Blockchain and Cryptocurr Tech",
+      category: "Wednesday",
+      description:
+        "3:00 PM - 3:50 PM - SJT415 - A2+TA2 - Poornima N",
+    },
+    {
+      id: 4,
+      image: "images/ISAA.png",
+      name: "Info Security Analysis and Audit",
+      category: "Wednesday",
+      description:
+        "4:00 PM - 4:50 PM - F2 - SJT208 - Sivakumar N",
+    },
+    {
+      id: 5,
+      image: "images/STS.jpg",
+      name: "STS",
+      category: "Wednesday",
+      description:
+        "5:00 PM - 5:50 PM - D2+TD2 - SJT423",
+    },
+
+    {
+      id: 1,
+      image: "images/IWP.jpeg",
+      name: "Internet and Web Programming (Lab)",
+      category: "Thrusday",
+      description:
+        "8:00 AM - 9:40 AM - L19 + L20 - SJT416 - Rajkumar R",
+    },
+    {
+      id: 2,
+      image: "images/German.jpg",
+      name: "German",
+      category: "Thrusday",
+      description:
+        "10:00 AM - 10:50 AM - G1 - SJT126 - Dhananjay Kumar",
+    },
+    {
+      id: 3,
+      image: "images/STS.jpg",
+      name: "STS",
+      category: "Thrusday",
+      description:
+        "2:00 PM - 2:50 PM - D2+TD2 - SJT423",
+    },
+    {
+      id: 4,
+      image: "images/SE.jpg",
+      name: "Software Engineering",
+      category: "Thrusday",
+      description:
+        "3:00 PM - 3:50 PM - B2 - SJT311 - Anto S",
+    },
+    {
+      id: 5,
+      image: "images/AI.jpg",
+      name: "Artificial Intelligence",
+      category: "Thrusday",
+      description:
+        "5:00 PM - 5:50 PM - E2+TE2 - SJT617 - Tusar Kanti Mishra",
+    },
+
+    {
+      id: 1,
+      image: "images/AI.jpg",
+      name: "Artificial Intelligence",
+      category: "Friday",
+      description:
+        "2:00 PM - 2:50 PM - E2+TE2 - SJT617 - Tusar Kanti Mishra",
+    },
+    {
+      id: 2,
+      image: "images/IWP.jpeg",
+      name: "Internet and Web Programming",
+      category: "Friday",
+      description:
+        "3:00 PM - 3:50 PM - C2 - SJT304 - Rajkumar R",
+    },
+    {
+      id: 3,
+      image: "images/Blockchain.jpg",
+      name: "Blockchain and Cryptocurr Tech",
+      category: "Friday",
+      description:
+        "4:00 PM - 4:50 PM - SJT415 - A2+TA2 - Poornima N",
+    },
+
+  ];
+  
+  export default Menu;
+  
